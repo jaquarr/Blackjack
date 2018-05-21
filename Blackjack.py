3c7ef0e032be8b2bc976901316159831e56b3e3e
jaquarr <39503588+jaquarr@users.noreply.github.com>
2018-05-21 18:49:29 -0400

    Updated from Brython Server: 5/21/2018 6:49:29 PM

--- Blackjack.py
+++ Blackjack.py
@@ -1,1 +1,21 @@
+#Jack Robey
+#5/21/18
+#Blackjack.py
+
+amountP1 = 5000
+
+wager = int(input('You have $5000. How much would you like to wager? The minimum is $2 and the maximum is $500. '))
+
+while True:
+    if wager < 2:
+        print()
+        wager = int(input('That wager is too low. The wager must at least be $2, so enter a different wager. '))
+        if wager > 2:
+            break
+    if wager > 500:
+        print()
+        wager = int(input('That wager is too high. The wager must at most be $500, so enter a different wager. '))
+        if wager < 500:
+            break
+
 
