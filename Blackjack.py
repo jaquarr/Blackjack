8f135e6ad7176121f91cb8c764d50d7b67bdf493
jaquarr <39503588+jaquarr@users.noreply.github.com>
2018-06-24 13:47:37 -0400

    Updated from Brython Server: 6/24/2018 1:47:37 PM

--- Blackjack.py
+++ Blackjack.py
@@ -16,7 +16,7 @@ if wager < 2:
 if wager > 500:
     while True:
         print()
-        wager = int(input('That wager does not meet with the restrictions ($2 is the minimum, $500 is the maximum). Enter another wager. '))
+        wager = int(input('That wager does not meet the restrictions ($2 is the minimum, $500 is the maximum). Enter another wager. '))
         if wager <= 500 and wager >= 2:
             break
 if wager >= 2 and wager <= 500:
@@ -28,7 +28,7 @@ if wager >= 2 and wager <= 500:
         if deck[x] == 0:
             deck[x] = i
         elif deck[x] != 0:
-            for j in range(0,51):
+            for j in range(-1,51):
                 j = j + 1
                 if deck[j] == 0:
                     deck[j] = i
